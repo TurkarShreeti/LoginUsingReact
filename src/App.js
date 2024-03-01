b27528dcdd6e8e5772e3eba15d4345d062800bd8
TurkarShreeti <shreetiturkar999@gmail.com>
2024-03-01 15:03:50 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+
+import { BrowserRouter, useHistory} from 'react-router-dom';
+import LoginSignup from './components/LoginSignup/LoginSignup';
+import Home from './components/Home/Home';
+import  './App.css';
+// import Home from './components/Home/Home';
+
+// import LoginSignup from './components/LoginSignup/LoginSignup';
+
+
 
 function App() {
+  
+
+  const[allEntry,setAllEntry]= useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <LoginSignup setAllEntry={setAllEntry} />
+        <Home allEntry={allEntry} />
+      </BrowserRouter>
     </div>
+   
   );
-}
+  }
 
 export default App;
